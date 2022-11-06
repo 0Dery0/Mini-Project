f3bf3e6ebefb5a7eac7cd39d84e2b6b80824b1d0
Dery <mderyd00@gmail.com>
2022-11-06 20:04:22 +0700

    Update fitur help,mr.d

--- src/Component/HelpPage.jsx
+++ src/Component/HelpPage.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Accordion } from 'react-bootstrap'
-
+import FooterWeb from './FooterWeb'
 import UserNavbar from './User/Navbar/UserNavbar'
 
 const HelpPage = () => {
@@ -19,12 +19,17 @@ const HelpPage = () => {
     <Accordion.Item eventKey="1">
       <Accordion.Header>Pertanyaan biasa:</Accordion.Header>
         <Accordion.Body>
-        -Stuck pada page?<br/>
-            Refresh page tersebut
+        -Loading lama?<br/>
+            Cek koneksi internetmu
         </Accordion.Body> 
     </Accordion.Item>
   </Accordion>
+  </div><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+  <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+  <div className=''>
+  <FooterWeb/>
   </div>
+  
     </>
     
   )
